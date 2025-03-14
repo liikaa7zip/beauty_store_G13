3c811730ae87e83a4f11b7d270ed1bbf69c9666d
Sreyka <honsreyka6@gmail.com>
2025-03-15 02:09:56 +0700

    fix(Display stock products):fix style that has error [BEATU-149]

--- views/inventory/products.php
+++ views/inventory/products.php
@@ -1,3 +1,15 @@
+<?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+// Redirect if not logged in
+if (!isset($_SESSION['user_id'])) {
+    header("Location: /users/signIn");
+    exit();
+}
+?>
+
 <div class="products_container">
     <h1 id="h1-products">Products Page</h1>
 
