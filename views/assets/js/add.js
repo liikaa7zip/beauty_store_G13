2b7a65fcf8cab3f72a0b5e0001152e8bc4085535
Kimchhik Chhuong <kimchhik.chhuong@student.passerellesnumeriques.org>
2025-03-15 13:35:42 +0700

    style(add products):display add products form [BEAUT-120]

--- views/assets/js/add.js
+++ views/assets/js/add.js
@@ -1,9 +1,33 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     var modal = document.getElementById("addProductModal");
+//     var openModalBtn = document.getElementById("addProductBtn"); // The button to open modal
+//     var closeModalBtn = document.querySelector(".close");
+
+//     // Open Modal when clicking the "Add Product" button
+//     openModalBtn.addEventListener("click", function () {
+//         modal.classList.add("open"); // Adds the 'open' class to make it visible
+//     });
+
+//     // Close Modal when clicking the 'X' button
+//     closeModalBtn.addEventListener("click", function () {
+//         modal.classList.remove("open"); // Removes 'open' class to hide modal
+//     });
+
+//     // Close Modal when clicking outside of it
+//     window.addEventListener("click", function (event) {
+//         if (event.target === modal) {
+//             modal.classList.remove("open");
+//         }
+//     });
+// });  
+
+
 document.addEventListener("DOMContentLoaded", function () {
     var modal = document.getElementById("addProductModal");
-    var openModalBtn = document.getElementById("addProductBtn"); // The button to open modal
+    var openModalBtn = document.querySelector(".icon.add"); // Use the '➕' icon to open modal
     var closeModalBtn = document.querySelector(".close");
 
-    // Open Modal when clicking the "Add Product" button
+    // Open Modal when clicking the "Add products" icon
     openModalBtn.addEventListener("click", function () {
         modal.classList.add("open"); // Adds the 'open' class to make it visible
     });
@@ -19,4 +43,4 @@ document.addEventListener("DOMContentLoaded", function () {
             modal.classList.remove("open");
         }
     });
-});  
+});
