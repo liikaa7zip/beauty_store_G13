e28862b7ac1bfa760036f976bf0c54d0005ebea6
Mean Un <mean.un.personal@gmail.com>
2025-03-20 09:56:45 +0700

    style(promotion style):fix promotion style [BEAUT-116]

--- views/promotion/promotion.php
+++ views/promotion/promotion.php
@@ -38,7 +38,7 @@ if (!isset($_SESSION['user_id'])) {
     <?php else: ?>
     <?php endif; ?>
 </div>
-<div class="promotion-container grid">
+<div class="promotion-container m-0">
     <?php foreach ($promotions as $promotion): ?>
         <div class="promotion-card">
             <div class="promotion-header"><?= htmlspecialchars($promotion['promotion_name']) ?></div>
