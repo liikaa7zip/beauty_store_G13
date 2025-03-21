36726d2e5d2f5cb091132aa7aebae17ca8623388
retthy <retthy.dork@student.passerellesnumeriques.org>
2025-03-21 19:15:28 +0700

    (fig code)

--- views/layouts/navbar.php
+++ views/layouts/navbar.php
@@ -9,64 +9,6 @@
         <a class="nav-link" data-lte-toggle="sidebar" href="#" role="button">
           <i class="bi bi-list"></i>
         </a>
-<<<<<<< HEAD
-      </div>
-      <div class="sidebar-wrapper bg-white">
-        <nav class="mt-2">
-          <ul class="nav sidebar-menu flex-column" data-lte-toggle="treeview" role="menu" data-accordion="false">
-            <li class="nav-item menu-open">
-              <a href="/dashboard/sell" class="nav-link active">
-                <i class="nav-icon bi bi-speedometer"></i>
-                <p>Dashboard</p>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a href="#" class="nav-link">
-                <i class="nav-icon bi bi-box-seam-fill"></i>
-                <p>Inventory<i class="nav-arrow bi bi-chevron-right"></i></p>
-              </a>
-              <ul class="nav nav-treeview">
-                <li class="nav-item"><a href="/inventory/products" class="nav-link"><i class="nav-icon bi bi-circle"></i>
-                    <p>Products</p>
-                  </a></li>
-                <li class="nav-item"><a href="#" class="nav-link"><i class="nav-icon bi bi-circle"></i>
-                    <p>Categories</p>
-                  </a></li>
-              </ul>
-            </li>
-            <li class="nav-item"><a href="/promotion" class="nav-link"><i class="nav-icon bi bi-table"></i>
-                <p>Promotions</p>
-              </a></li>
-            <li class="nav-item"><a href="#" class="nav-link"><i class="nav-icon bi bi-clipboard-fill"></i>
-                <p>Expiration</p>
-              </a></li>
-            <li class="nav-item"><a href="" class="nav-link"><i class="nav-icon bi bi-tree-fill"></i>
-                <p>Notification</p>
-              </a></li>
-            <li class="nav-item"><a href="#" class="nav-link"><i class="nav-icon bi bi-clipboard-fill"></i>
-                <p>Histories</p>
-              </a></li>
-            <li class="nav-item">
-              <a href="#" class="nav-link">
-                <i class="nav-icon bi bi-box-arrow-in-right"></i>
-                <p>Auth<i class="nav-arrow bi bi-chevron-right"></i></p>
-              </a>
-              <ul class="nav nav-treeview">
-                <li class="nav-item"><a href="/users/signUp" class="nav-link"><i class="nav-icon bi bi-box-arrow-in-right"></i>
-                    <p>SignOut</p>
-                  </a></li>
-              </ul>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </aside>
-  <!-- </div> -->
-  <main class="app-main">
-  <?php
-endif;
-  ?>
-=======
       </li>
       <li class="nav-item d-none d-md-block"><a href="#" class="nav-link">Home</a></li>
       <li class="nav-item d-none d-md-block"><a href="#" class="nav-link">Contact</a></li>
@@ -248,4 +190,3 @@ endif;
   </div>
   <!--end::Container-->
 </nav>
->>>>>>> main
