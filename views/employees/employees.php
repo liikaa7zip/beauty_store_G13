7b148920e7438c93b1cb40e54e853a247aeda7fd
Sreyka <honsreyka6@gmail.com>
2025-03-21 15:22:01 +0700

    fix(fix style):fix the style [BEAUT-9]

--- views/employees/employees.php
+++ views/employees/employees.php
@@ -1,15 +1,14 @@
-<div class="employee-container" id="employee-container">
-        <header class="header" id="employee-header">
+<div class="employee-section" id="employee-section">
+        <header class="employee-header" id="employee-header">
             <div class="header-left">
-                <input type="text" class="search-box" id="search-employee" placeholder="Search Employee">
+                <input type="text" class="search-employ" id="search-employee" placeholder="Search Employee">
             </div>
-            <div class="header-right">
-                <button class="add-btn" id="add-employee">Add <i class="fas fa-plus-circle"></i></button>
-                <button class="import-btn" id="import-employee">Import <i class="fas fa-file-import"></i></button>
-
+            <div class="header-right action-buttons">
+                <button class="add-employee-btn" id="add-employee">Add <i class="fas fa-plus-circle"></i></button>
+                <button class="import-employee-btn" id="import-employee">Import <i class="fas fa-file-import"></i></button>
             </div>
         </header>
-        <table class="employee-table" id="employee-table">
+        <table class="employee-list-table" id="employee-list-table">
             <thead class="table-header" id="table-header">
                 <tr>
                     <th class="table-heading" id="col-name">Name</th>
@@ -28,7 +27,7 @@
                             <td class="table-data" id="data-name-<?= $employee['id'] ?>"><?= $employee['username'] ?></td>
                             <td class="table-data" id="data-department-<?= $employee['id'] ?>"><?= $employee['role'] ?></td>
                             <td class="table-data" id="data-contact-<?= $employee['id'] ?>"><?= $employee['contact'] ?></td>
-                                <td class="table-data" id="data-requests-<?= $employee['id'] ?>"><?= $employee['status'] ?></td>
+                            <td class="table-data" id="data-status-<?= $employee['id'] ?>"><?= $employee['status'] ?></td>
                         </tr>
                     <?php endforeach;
                 } else {
