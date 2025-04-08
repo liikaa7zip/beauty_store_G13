6ac083337825b1be969aeba57d00b86b56963299
retthy <retthy.dork@student.passerellesnumeriques.org>
2025-04-08 19:29:51 +0700

    fix(feature/telegramalert): updated style in page notification [BEAUT-7]

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -57,6 +57,7 @@
   <link rel="stylesheet" href="/views/assets/css/addemployees.css">
   <link rel="stylesheet" href="/views/assets/css/dashboard.css">
   <link rel="stylesheet" href="/views/assets/css/UserHistory.css">
+  <link rel="stylesheet" href="/views/assets/css/notification.css">
 
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
   <link rel="preconnect" href="https://fonts.googleapis.com">
