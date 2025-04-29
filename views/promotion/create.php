1703153b96513c4c9d0129706851614294030eac
Sreyka <honsreyka6@gmail.com>
2025-04-29 12:14:11 +0700

    fix

--- views/promotion/create.php
+++ views/promotion/create.php
@@ -132,3 +132,5 @@ if (session_status() == PHP_SESSION_NONE) {
 </script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css">
+
+
